ca00ab8b900661906a6a8c0719820073aa6f36ef
Shalamar <borribes@gmail.com>
2015-01-23 10:41:13 +0100

    nuevo

---
+++ Ejercicios/src/Clases/Punto.java
@@ -0,0 +1,28 @@
+package Clases;
+
+public class Punto {
+	public int x;
+	public int y;
+	public int e;
+	
+	public Punto (){
+		x=0;
+		y=0;
+	}
+	                   
+	public Punto (int numX, int numY){
+		x=numX;
+		y=numY;
+	}
+	public int  getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void printCoordenadas(){
+		
+	}
+}
